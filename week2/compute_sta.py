bd043bea977aad0241b5eeab4eb8766272c0b266
Simon Crase <simon@greenweaves.nz>
2022-11-09 15:20:39 +1300

    #1 plot raw data

--- week2/compute_sta.py
+++ week2/compute_sta.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Created on Wed Apr 22 15:21:11 2015
 
@@ -5,22 +6,21 @@ Code to compute spike-triggered average.
 """
 
 from __future__ import division
-import numpy as np
-import matplotlib.pyplot as plt
+from numpy      import zeros
 
-
+TBD=0
 def compute_sta(stim, rho, num_timesteps):
     """Compute the spike-triggered average from a stimulus and spike-train.
-    
+
     Args:
         stim: stimulus time-series
         rho: spike-train time-series
         num_timesteps: how many timesteps to use in STA
-        
+
     Returns:
         spike-triggered average for num_timesteps timesteps before spike"""
-    
-    sta = np.zeros((num_timesteps,))
+
+    sta = zeros((num_timesteps,))
 
     # This command finds the indices of all of the spikes that occur
     # after 300 ms into the recording.
@@ -28,8 +28,8 @@ def compute_sta(stim, rho, num_timesteps):
 
     # Fill in this value. Note that you should not count spikes that occur
     # before 300 ms into the recording.
-    num_spikes = 
-    
+    num_spikes = TBD
+
     # Compute the spike-triggered average of the spikes found.
     # To do this, compute the average of all of the vectors
     # starting 300 ms (exclusive) before a spike and ending at the time of
@@ -37,7 +37,7 @@ def compute_sta(stim, rho, num_timesteps):
     # samples that is contained within a window of 300 ms before each
     # spike. The average of these vectors should be completed in an
     # element-wise manner.
-    # 
+    #
     # Your code goes here.
-    
+
     return sta
--- week2/quiz2.py
+++ week2/quiz2.py
@@ -1,38 +1,38 @@
+#!/usr/bin/env python
 """
 Created on Wed Apr 22 15:15:16 2015
 
 Quiz 2 code.
 """
 
-from __future__ import division
-import numpy as np
-import matplotlib.pyplot as plt
-
-import pickle
-
-from compute_sta import compute_sta
-
+from __future__        import division
+from numpy             import arange
+from matplotlib.pyplot import figure, show
+from pickle            import load
+from compute_sta       import compute_sta
 
 FILENAME = 'c1p8.pickle'
 
 with open(FILENAME, 'rb') as f:
-    data = pickle.load(f)
+    data = load(f)
+    stim            = data['stim']
+    rho             = data['rho']
+    N               = 100
+    sampling_period = 2 # in ms
+    num_timesteps   = 148
+    sta             = compute_sta(stim, rho, num_timesteps)
+    time            = (arange(-num_timesteps, 0) + 1) * sampling_period
 
-stim = data['stim']
-rho = data['rho']
-
-
-# Fill in these values
-sampling_period = # in ms
-num_timesteps = 
-
-sta = compute_sta(stim, rho, num_timesteps)
-
-time = (np.arange(-num_timesteps, 0) + 1) * sampling_period
-
-plt.plot(time, sta)
-plt.xlabel('Time (ms)')
-plt.ylabel('Stimulus')
-plt.title('Spike-Triggered Average')
-
-plt.show()
+    fig             = figure(figsize=(10,10))
+    ax1             = fig.add_subplot(2,1,1)
+    ax1a            = ax1.twinx()
+    ax2             = fig.add_subplot(2,1,2)
+    ax1.plot (stim[0:N],c='xkcd:blue')
+    ax1a.plot (rho[0:N],'ro')
+    ax1.set_ylabel('stim',c='xkcd:blue')
+    ax1a.set_ylabel('rho',c='xkcd:red')
+    ax2.plot(time, sta)
+    ax2.set_xlabel('Time (ms)')
+    ax2.set_ylabel('Stimulus')
+    ax2.set_title('Spike-Triggered Average')
+    show()
