45553c6ea852ea00de15acdb2b7dda6f8c0ad44d
Simon Crase <simon@greenweaves.nz>
2022-12-21 15:03:20 +1300

    #2 WIP

--- week4/poisson.py
+++ week4/poisson.py
@@ -1,25 +1,34 @@
 from matplotlib.pyplot import figure, show
-from numpy             import mean, var
+from numpy             import array, mean, var
 from pickle            import load
 
-with open('tuning.pickle', 'rb') as f:
-    data = load(f)
-    print (data.keys())
+def get_tuning():
+    with open('tuning.pickle', 'rb') as f:
+        data   = load(f)
+        stim   = data['stim']
+        Neuron = array([data[f'neuron{i+1}'] for i in range(4)])
+    return stim,Neuron
 
-    print (data['stim'].shape)
-    print (data['stim'])
-    print (data['neuron1'].shape)
-    print (mean(data['neuron1'],axis=0).shape)
+def get_pop_coding():
+    with open('pop_coding.pickle', 'rb') as  f:
+        data = load(f)
+        C    = array([data[f'c{i+1}'] for i in range(4)])
+        R    = array([data[f'r{i+1}'] for i in range(4)])
+        return C, R
+
+if __name__=='__main__':
+    stim,Neuron = get_tuning()
+    C,R         = get_pop_coding()
+
     fig = figure(figsize=(8,8))
     ax1 = fig.add_subplot(2,1,1)
     ax2 = fig.add_subplot(2,1,2)
     for i in range(4):
-        neuron = f'neuron{i+1}'
-        spike = 1/data[neuron]
-        ax1.plot(mean(data[neuron],axis=0),label=neuron)
-        ax2.plot(mean(spike,axis=0)/var(spike,axis=0),label=neuron)
+        spike = 1/Neuron[i,:,:]
+        ax1.plot(mean(Neuron[i,:,:],axis=0),label=f'Tuning: {i+1}')
+        ax2.plot(mean(spike,axis=0)/var(spike,axis=0),label=f'{i+1}')
     ax1.legend()
 
-with open('pop_coding.pickle', 'rb') as  f1:
-    data = load(f1)
-show()
+
+
+    show()
